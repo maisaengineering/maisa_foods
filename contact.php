0eefd1983e2ba1665343aa8f206c09ed3e60ddb7
Chandan Benjaram <chandan.benjaram@gmail.com>
2020-01-27 03:14:17 -0800

    clean-up

--- contact.php
+++ contact.php
@@ -4,13 +4,12 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Maisa Spices Maisa Garam Masala is made from selected spices and is prepared under most hygienic conditions.</title>
 <meta name="language" content="EN" />
-<meta name="description" content="Maisa Garam Masala is made from selected spices and is prepared under most hygienic conditions.Maisa Garam Masala makes very dish tastes special with its rich aroma and flavour.It is a perfect blend of spices so as you can use it in all types of curries.Add Maisa Garam Masala just before you finish cooking to ensure that its subtle flavour and aroma are retained.spices instantFoods blendedSpices spices maisamasalas garam masala vegetarian masala non-vegetarian masala chicken masala mutton masala sambar powder rasam powder chicken 65 masala chicken kabab masala "/>
+<meta name="description" content="Maisa spices, hand picked from planets best places, are prepared under most hygienic conditions. Instantly enjoy delicious flavour to your food!"/>
 <meta name="keywords" content="spices,instantfoods,blendedSpices,spices,maisamasalas,chicken nuggets,chicken crackers,garam masala,vegetarian masala,non-vegetarian masala,chicken masala,mutton masala,sambar powder,rasam powder,chicken 65 masala,chicken kabab masala,biryani masala,instant chicken masala,ginger and garlic paste" />
-<meta name="author" content="maisa.co.in"/>
+<meta name="author" content="MaisaFoods.com"/>
 <meta name="robots" content="index, follow"/>
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
 <link href="images/fav.png" rel="icon" type="image/gif" />
-
 <script src="js/jquery.js" type="text/javascript" language="javascript"></script>
 <script type="text/javascript" language="javascript" src="js/script.js"></script>
 </head>
@@ -26,18 +25,15 @@
               </div>
             </div>
                   <ul class="menu" id="menu">
-            
+
                 <li><a href="index.html" class="menulink">Home</a></li>
                 <li class="devider"><img src="images/spacer.gif" /></li>
-                <li><a href="aboutus.html" class="menulink">About Us</a></li>
+                <li><a href="products.html" class="menulink">Products</a></li>
                 <li class="devider"><img src="images/spacer.gif" /></li>
-                <li><a href="products.html" class="menulink">Products</a>
-                </li>
-                <li class="devider"><img src="images/spacer.gif" /></li>
-                <li><a href="News.html" class="menulink">News & Events</a></li>
+                <li><a href="News.html" class="menulink">In the Media</a></li>
                 <li class="devider"><img src="images/spacer.gif" /></li>
                 <li class="menu_active"><a href="contact.php" class="menulink">Contact Us</a></li>
-                
+
             </ul>
             <script type="text/javascript">
                 var menu=new menu.dd("menu");
@@ -62,64 +58,27 @@
     <div class="content_wrapper_right">
         <div class="banner"><img src="images/banners/inner_banner1.png" border="0" /></div>
         <div class="content-area">
-		<h2>Address</h2>
+		<h2>Corporate Office:</h2>
 		 <img src="images/contact.png" style="float:right; width: 50%; margin-top: -20px;" />
 		    <p>
-			   <strong style="font-size:14px;">Maisa Foods</strong><br/>
-			   Plot No. 26, New vasavishiva nagar <br/>
-			   Kushaiguda, Ecil,Hyderabad-500062<br/>
-			   Phone No: 040-40177227, <br/>
+			   <strong style="font-size:14px;">Maisa Food Product Pvt Ltd.</strong><br/>
+			   Plot No. 26, New Vasavishiva Nagar <br/>
+			   Kushaiguda, Ecil, Hyderabad, Telangana - 500062<br/><br/>
+
+			   Email: maisafoods@gmail.com
+				 Phone No: 040-40177227 <br/>
 			   Customer Service No: 1800 3010 0609<br/>
-			   Email: maisafoods@gmail.com 
 			 </p>
-			 <h2>Contact Form</h2>
-		
-        	<form action="contact.php" method="post">
-							<table width="400" border="0" cellspacing="2" cellpadding="0">
-							<tr>
-							<td width="40%" class="bodytext">Your name:</td>
-							<td width="60%"><input name="name" type="text" id="name" size="32"></td>
-							</tr>
-							<tr>
-							<td  width="50%"class="bodytext">Email address:</td>
-							<td width="50%"><input name="email" type="text" id="email" size="32"></td>
-							</tr>
-							<tr>
-							<td width="40%" class="bodytext">Message: </td>
-							<td width="60%"><textarea name="msg" cols="35" rows="3" id="msg" class="bodytext"></textarea></td>
-							</tr>
-							<tr>
-							<td class="bodytext"> </td>
-							<td align="left" valign="top"><input type="submit" name="Submit" value="Send"></td>
-							</tr>
-							</table>
-						</form>
-						
-							<?php 
-	$emailTo = 'maisafoods@gmail.com';
-	if($subject!=''){
-	$subject =$_REQUEST['subject'];
-	}else{
-	$subject = 'Contacting for Masala Powders';
-	}
-	$name=$_REQUEST['name'];
-	$email=$_REQUEST['email'];
-	$msg=$_REQUEST['msg'];
-	
-	$body = "Name: $name \n\nEmail: $email \n\nMessage: $msg";
-	$headers = 'From: '.$name.' <'.$email.'>' . "\r\n" . 'Reply-To: ' . $email;
-	
-	mail($emailTo, $subject, $body, $headers);
-?>
+			 <h3>***DISTRIBUTORS WANTED. PLEASE CONTACT US AT THE EARLIEST.</h3>
         </div>
     </div>
     <div class="cls"></div>
 </div>
 <div class="footer">
         <div class="footer-wrapper">
-            <div class="left">&copy; 2015 Maisa&#8482; Foods. All rights reserved.</div>
+            <div class="left">Maisa&#8482; registered trademark of Maisa Food Products Pvt Ltd.</div>
             <div class="right"><a href="http://www.maisasolutions.com" target="new"><img src="images/maisasolutions.png" width="30px" title="Maisa Solutions"></a></div>
-            <div class="right">Designed & Developed by &nbsp; </div> 
+            <div class="right">Designed & Developed by &nbsp; </div>
             <div class="cls"></div>
         </div>
     </div>
--- index.php
+++ index.php
@@ -4,18 +4,18 @@
 
 <?php
   $id=$_GET['id'];
-  if($id=="") $id=1; 
-?>	
+  if($id=="") $id=1;
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 		<head>
 		<title>Maisa&#8482; Foods Maisa Garam Masala is made from selected spices and is prepared under most hygienic conditions.</title>
 		<meta name="language" content="EN" />
-			<meta name="description" content="Maisa Garam Masala is made from selected spices and is prepared under most hygienic conditions.Maisa Garam Masala makes very dish tastes special with its rich aroma and flavour.It is a perfect blend of spices so as you can use it in all types of curries.Add Maisa Garam Masala just before you finish cooking to ensure that its subtle flavour and aroma are retained.spices instantFoods blendedSpices spices maisamasalas garam masala vegetarian masala non-vegetarian masala chicken masala mutton masala sambar powder rasam powder chicken 65 masala chicken kabab masala "/>
-			<meta name="keywords" content="spices,instantfoods,blendedSpices,spices,maisamasalas,chicken nuggets,chicken crackers,garam masala,vegetarian masala,non-vegetarian masala,chicken masala,mutton masala,sambar powder,rasam powder,chicken 65 masala,chicken kabab masala,biryani masala,instant chicken masala,ginger and garlic paste" />
-			<meta name="author" content="maisafoods.com"/>
-			<meta name="robots" content="index, follow"/>
+    <meta name="description" content="Maisa spices, hand picked from planets best places, are prepared under most hygienic conditions. Instantly enjoy delicious flavour to your food!"/>
+		<meta name="keywords" content="spices,instantfoods,blendedSpices,spices,maisamasalas,chicken nuggets,chicken crackers,garam masala,vegetarian masala,non-vegetarian masala,chicken masala,mutton masala,sambar powder,rasam powder,chicken 65 masala,chicken kabab masala,biryani masala,instant chicken masala,ginger and garlic paste" />
+		<meta name="author" content="maisafoods.com"/>
+		<meta name="robots" content="index, follow"/>
 		<?php include ('analyticstracking.php'); ?>
 		<link rel="stylesheet" type="text/css" href="./css/styles.css">
 		            <script type="text/javascript" src="js/modernizr.js"></script>
@@ -32,14 +32,14 @@
 	<?php include 'spices_masalas_slider.php'; ?>
 </div>
 
-<div class="main_block">	
+<div class="main_block">
 <div class="block_one">
 	<br>
 	<img src="images/about_maisa.jpg">
 	    <div style="margin:10px;color:#000">
 	    	<br>
 <iframe width="280" height="280" src="//www.youtube.com/embed/VBJr5Wrwges" frameborder="0" allowfullscreen></iframe>        </div>
-</div>	
+</div>
 <div class="block_two">
 	<br>
 	<img src="images/products.jpg">
@@ -50,45 +50,44 @@
 margin-top: -60px;" border="0"></div>
 	 <script type="text/javascript">
              $(document).ready(function(e) {
-             $('#new1').delay(1000).fadeOut('slow');
+             $('#new1').delay(500).fadeOut('slow');
              });
 
         </script>
 		<marquee style="position: relative;margin-top:-50px;" behavior="scroll" align="center" direction="up" scrollamount="2" scrolldelay="5" onmouseover="this.stop()" onmouseout="this.start()" height="330"><br>
-
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/chilli.png" width="200" alt="chilli powder" title="chilli powder" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/coriander.png" width="200" alt="coriander powder" title="coriander powder" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/turmeric.png" width="200" alt="turmeric powder" title="turmeric powder" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/chickenmasala.jpg" width="200" alt="chicken masala" title="chicken masala" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/muttonmasala.jpg" width="200" alt="mutton masala" title="mutton masala" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/chicken65kababmasala.jpg" width="200" alt="chicken 65 and kabab masala" title="chicken 65 and kabab masala" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/rasampowder.jpg" width="200" alt="rasam powder" title="rasam powder" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/sambarpowder.jpg" width="200" alt="sambar powder" title="sambar powder" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/garammasala.jpg" width="200" alt="garam masala" title="garam masala" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/biryanimasala.jpg" width="200" alt="biryani masala" title="biryani masala" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/instantchickenmasala.jpg" width="200" alt="instant chicken masala" title="instant chicken masala" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="maisa_instantfood.php" target="_blank"><img src="images/chickencrackers.jpg" width="200" alt="chicken crackers" title="chicken crackers" style="position: relative;" border="0"></a><br></center>
-		 <br>
-		 <center><a href="maisa_instantfood.php" target="_blank"><img src="images/chickennuggets.jpg" width="200" alt="chicken nuggets" title="chicken nuggets" style="position: relative;" border="0"></a><br></center>
-        </marquee>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/chilli.png" width="200" alt="chilli powder" title="chilli powder" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/coriander.png" width="200" alt="coriander powder" title="coriander powder" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/turmeric.png" width="200" alt="turmeric powder" title="turmeric powder" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/chickenmasala.jpg" width="200" alt="chicken masala" title="chicken masala" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/muttonmasala.jpg" width="200" alt="mutton masala" title="mutton masala" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/chicken65kababmasala.jpg" width="200" alt="chicken 65 and kabab masala" title="chicken 65 and kabab masala" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/rasampowder.jpg" width="200" alt="rasam powder" title="rasam powder" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/sambarpowder.jpg" width="200" alt="sambar powder" title="sambar powder" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/garammasala.jpg" width="200" alt="garam masala" title="garam masala" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/biryanimasala.jpg" width="200" alt="biryani masala" title="biryani masala" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="masalas_spices_products.php" target="_blank"><img src="images/instantchickenmasala.jpg" width="200" alt="instant chicken masala" title="instant chicken masala" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="maisa_instantfood.php" target="_blank"><img src="images/chickencrackers.jpg" width="200" alt="chicken crackers" title="chicken crackers" style="position: relative;" border="0"></a><br></center>
+  		 <br>
+  		 <center><a href="maisa_instantfood.php" target="_blank"><img src="images/chickennuggets.jpg" width="200" alt="chicken nuggets" title="chicken nuggets" style="position: relative;" border="0"></a><br></center>
+    </marquee>
 	<br>
 	<div style="margin:10px;color:#000">
 	    	<!-- <ul>
 	    		Introducing Chicken Crackers
 	    		<li></li>
             </ul> -->
-           
+
         </div>
 </div>
 <!-- lightbox -->
@@ -99,14 +98,14 @@ margin-top: -60px;" border="0"></div>
     <div class="recipes_box">
     	<a href="chicken_curry_recipe.php">
     	<img src="images/chicken-curry-recipe.jpg" align="left" width="80"><p><b>Chicken Curry</b><br> made with maisa masala<br>for details Click here</p>
-    	</a> 
+    	</a>
     </div>
     <hr>
     <div class="recipes_box">
     	<a href="chicken_seekh_kebabs_recipe.php">
     	<img src="images/chicken-seekh-kebab-recipe.jpg" align="left" width="80"><p><b>Chicken Seekh Kebab</b><br> made with maisa masala<br>for details Click here</p>
         </a>
-    </div>	
+    </div>
     <hr>
     <div class="recipes_box">
     	<a href="egg_curry_recipe.php">
@@ -121,76 +120,75 @@ margin-top: -60px;" border="0"></div>
 <div class="clear"></div>
 		<?php include 'footer.php'; ?>
 	    </div>
-        
-        
+
+
  <!--------------------------------------------------popup----------------------------------->
- <?php 
-	if(!isset($_SESSION['jquery_popup']))  { 
+ <?php
+	if(!isset($_SESSION['jquery_popup']))  {
 	$_SESSION['jquery_popup'] = 1;
 	?>
 <script type="text/javascript">
-	$(document).ready(function() {	
-	
+	$(document).ready(function() {
+
 			var id = '#dialog';
-		
+
 			//Get the screen height and width
 			var maskHeight = $(document).height();
 			var maskWidth = $(window).width();
-		
+
 			//Set heigth and width to mask to fill up the whole screen
 			$('#mask').css({'width':maskWidth,'height':maskHeight});
-			
-			//transition effect		
-			$('#mask').fadeIn(800);	
-			$('#mask').fadeTo("slow",0.8);	
-		
+
+			//transition effect
+			$('#mask').fadeIn(800);
+			$('#mask').fadeTo("slow",0.8);
+
 			//Get the window height and width
 			var winH = $(window).height();
 			var winW = $(window).width();
-				  
+
 			//Set the popup window to center
 			$(id).css('top',  winH/2-$(id).height()/2 -50);
 			$(id).css('left', winW/2-$(id).width()/2);
-		
+
 			//transition effect
-			$(id).fadeIn(500); 	
-		
+			$(id).fadeIn(500);
+
 		//if close button is clicked
 		$('.window .close').click(function (e) {
 			//Cancel the link behavior
 			e.preventDefault();
-			
+
 			$('#mask').hide();
 			$('.window').hide();
-		});		
-		
+		});
+
 		//if mask is clicked
 		$('#mask').click(function () {
 			$(this).preventDefault();
 			$(this).hide();
 			$('.window').hide();
-		});		
-		
+		});
+
 	});
-	
+
 	</script>
 <div id="boxes">
           <div style="top:150px; left: 551.5px; display: none;" id="dialog" class="window">
     <div align="right" style="font-weight:bold; margin:5px 3px 0 0;"><a href="javascript:void()" class="close"><img src="images/close.png" width="16" style="border:none; cursor:pointer;" /></a></div>
     <div align="center" style="margin:5px 0 5px 0;">
-             
-            <div><a href="maisa_order_form.php"><img src="images/popup.jpg"width="910"></a></div> 
-             
+
+            <div><a href="maisa_order_form.php"><img src="images/popup.jpg"width="910"></a></div>
+
             </div>
-   
+
   </div>
-          
+
           <!-- Mask to cover the whole screen -->
           <div style="width: 2000px; height: 2000px; display: none; opacity: 0.7;" id="mask"></div>
         </div>
 <?php }
 	//if((isset($_SESSION['jquery_popup'])))  { unset($_SESSION['jquery_popup']); } //uncomment for testing
-	?>       
+	?>
 	</body>
 </html>
-
